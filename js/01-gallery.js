b9563d7272a4d9940f7c80b41ee248e8e60f0f18
Vas2306 <vasilisa230679@gmail.com>
2023-02-22 22:35:09 +0100

    Создание и рендер разметки по массиву данных

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const containerForImages = document.querySelector(".gallery");
+// console.log(containerForImages);
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+containerForImages.insertAdjacentHTML("beforeend", createGalleryMarkup);
