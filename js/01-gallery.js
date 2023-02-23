a81d072e0ceb57d450e0b474df2c473757160b4c
Vas2306 <vasilisa230679@gmail.com>
2023-02-23 11:31:53 +0100

    создание и рендер разметки по массиву для второго задания

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,13 +6,13 @@ const containerForImages = document.querySelector(".gallery");
 
 const createGalleryMarkup = galleryItems
   .map(
-    ({ preview, description, original }) =>
+    ({ preview, original, description }) =>
       `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
+    data-source="${original}"
       alt="${description}"
     />
   </a>
@@ -21,3 +21,23 @@ const createGalleryMarkup = galleryItems
   .join("");
 
 containerForImages.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+containerForImages.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+ }
+  evt.preventDefault();
+  const urlBigImage = evt.target.dataset.source;
+
+  console.log(urlBigImage);
+
+  console.log(evt.target);
+  console.log(evt.target.nodeName);
+  console.log(evt.target.classList.value);
+  console.log(evt.target.src);
+  console.log(evt.target.alt);
+  console.log(evt.target.dataset.source);
+};
+ 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const containerForImages = document.querySelector(".gallery");
+// console.log(containerForImages);
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+containerForImages.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+containerForImages.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+ }
+  evt.preventDefault();
+  const urlBigImage = evt.target.closest('.gallery__item').href;
+
+  console.log(urlBigImage);
+
+  console.log(evt.target);
+  console.log(evt.target.nodeName);
+  console.log(evt.target.classList.value);
+  console.log(evt.target.src);
+  console.log(evt.target.alt);
+ 
+};
