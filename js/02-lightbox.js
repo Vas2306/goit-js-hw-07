7280b11de496e75194fc9d0ad2f866b165ecf656
Vas2306 <vasilisa230679@gmail.com>
2023-02-23 16:06:07 +0100

    simplelightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,36 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-
-// console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
 
 const containerForImages = document.querySelector(".gallery");
-// console.log(containerForImages);
 
 const createGalleryMarkup = galleryItems
   .map(
     ({ preview, original, description }) =>
       `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
 </a>`
   )
   .join("");
 
 containerForImages.insertAdjacentHTML("beforeend", createGalleryMarkup);
 
-containerForImages.addEventListener('click', onImageClick);
-
 function onImageClick(evt) {
-  if (evt.target.nodeName !== 'IMG') {
+  if (evt.target.nodeName !== "IMG") {
     return;
- }
+  }
   evt.preventDefault();
-  const urlBigImage = evt.target.closest('.gallery__item').href;
+}
 
-  console.log(urlBigImage);
+containerForImages.addEventListener("click", onImageClick);
 
-  console.log(evt.target);
-  console.log(evt.target.nodeName);
-  console.log(evt.target.classList.value);
-  console.log(evt.target.src);
-  console.log(evt.target.alt);
- 
-};
+var lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
